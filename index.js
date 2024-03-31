714a036689d1c225e5c8dd9186be214f9eaa19ac
Dan Koskei <qimtaid@gmail.com>
2024-03-31 16:13:13 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+
+// find drivers 
+function findMatching(drivers, searchString) {
+    const lowercaseSearch = searchString.toLowerCase(); // Convert search string to lowercase for case-insensitive matching
+    return drivers.filter((driver) => driver.toLowerCase().includes(lowercaseSearch));
+  }
+  
+  //  Specific drivers 
+  function fuzzyMatch(drivers, searchString) {
+    const lowercaseSearch = searchString.toLowerCase();
+    return drivers.filter((driver) => driver.toLowerCase().startsWith(lowercaseSearch));
+  }
+  
+  // matching drivers names
+  function matchName(drivers, name) {
+    return drivers.filter((driver) => driver.name === name);
+  }
